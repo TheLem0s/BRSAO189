b525eb192242056028f0cde4c07775e7188915ef
Fulano <fulano@eita.com>
2025-06-24 20:15:08 -0300

    preco

---
+++ CalculadoraPreco.py
@@ -0,0 +1,10 @@
+produto = "Cadeira Infantil"
+preco = 12.40
+qtd = 3
+
+preco_total = preco * qtd
+
+print(produto)
+print(f"Valor unitário: {preco}")
+print(f"Quantidade: {qtd}")
+print(f"Valor Total: {preco_total}")
